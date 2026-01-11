c2d1165be89ef9cc0215c1514f7306753a030314
Zhengbao Lai <laizhengbao@icloud.com>
2026-01-11 09:16:15 +0800

    fix: building

--- FullBuild.sh
+++ FullBuild.sh
@@ -11,12 +11,14 @@ usage(){
 }
 
 MODULES(){
+	# you might need to put these in env.sh for efficiency
 	if module avail > /dev/null 2> /dev/null
 	then
 		module purge
 
 		# TWCC
 		module load nvhpc-24.11_hpcx-2.20_cuda-12.6
+		module load gcc10/10.2.1
 
 	else
 		echo "No modules, skipping loading"
@@ -24,14 +26,33 @@ MODULES(){
 
 	export CC=mpicc
 	export CXX=mpicxx
+
+	export OMPI_CC=gcc
+	export OMPI_CXX=g++
+	export OMPI_FC=gfortran
+
+	export CUVER=70
+	export CUDA_GRAPH=""
 }
 
 DEPbuild(){
+
+	if [ -d dep/build ]
+	then
+		return 0
+	fi
+
 	echo ""
 	echo "Start building dependencies..."
 	echo ""
 
-	cmake -B dep/build dep --fresh
+	cmake -B dep/build dep --fresh \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_C_COMPILER=mpicc \
+		-DCMAKE_CXX_COMPILER=mpicxx \
+		-DCMAKE_CUDA_COMPILER="$(which nvcc)" \
+		-DCMAKE_CUDA_HOST_COMPILER="$(which g++)" \
+		-DCMAKE_CUDA_ARCHITECTURES="${CUVER}" \
 	cmake --build dep/build -j
 }
 
@@ -43,39 +64,65 @@ SRCbuild(){
 	printf "Start building src with varient '%s'\n" "$VARIENT"
 	echo ""
 
+	set --
+
 	case "$VARIENT"
 	in
 		"PP")
-		OPTION="$(cat <<- EOF
-			-DHEMELB_USE_VELOCITY_WEIGHTS_FILE=OFF
-			-DHEMELB_INLET_BOUNDARY=NASHZEROTHORDERPRESSUREIOLET
-			-DHEMELB_WALL_INLET_BOUNDARY=NASHZEROTHORDERPRESSURESBB
-			-DHEMELB_OUTLET_BOUNDARY=NASHZEROTHORDERPRESSUREIOLET
+		set -- "$@" \
+			-DHEMELB_USE_VELOCITY_WEIGHTS_FILE=OFF \
+			-DHEMELB_INLET_BOUNDARY=NASHZEROTHORDERPRESSUREIOLET \
+			-DHEMELB_WALL_INLET_BOUNDARY=NASHZEROTHORDERPRESSURESBB \
+			-DHEMELB_OUTLET_BOUNDARY=NASHZEROTHORDERPRESSUREIOLET \
 			-DHEMELB_WALL_OUTLET_BOUNDARY=NASHZEROTHORDERPRESSURESBB
-			EOF
-		)"
 		;;
 		"VP")
-		OPTION="$(cat <<- EOF
-			-DHEMELB_USE_VELOCITY_WEIGHTS_FILE=ON 
-			-DHEMELB_INLET_BOUNDARY=LADDIOLET 
-			-DHEMELB_WALL_INLET_BOUNDARY=LADDIOLETSBB 
-			-DHEMELB_OUTLET_BOUNDARY=NASHZEROTHORDERPRESSUREIOLET 
-			-DHEMELB_WALL_OUTLET_BOUNDARY=NASHZEROTHORDERPRESSURESBB 
-			EOF
-		)"
+		set -- "$@" \
+			-DHEMELB_USE_VELOCITY_WEIGHTS_FILE=ON \
+			-DHEMELB_INLET_BOUNDARY=LADDIOLET \
+			-DHEMELB_WALL_INLET_BOUNDARY=LADDIOLETSBB \
+			-DHEMELB_OUTLET_BOUNDARY=NASHZEROTHORDERPRESSUREIOLET \
+			-DHEMELB_WALL_OUTLET_BOUNDARY=NASHZEROTHORDERPRESSURESBB \
 		;;
 		*)
-		OPTION=""
 		;;
 	esac
 
+	if [ -z "${CUDA_GRAPH}" ]
+	then
+		set -- "$@" \
+			-DHEMELB_USE_CUDA_GRAPH=OFF \
+			-DCMAKE_CUDA_FLAGS="--maxrregcount=64 --ptxas-options=-v"
+	fi
+
+	INSTALL_DIR="$(pwd)/hemelabgpu${VARIENT:+-${VARIENT}}"
+
+	if [ -d "${INSTALL_DIR}" ]
+	then
+		rm -rf "${INSTALL_DIR}"
+	fi
+
+	# NVC doesn't support cmake ipo detection
+	# and we shall not use -Mipa=fast, it would cause seg fault
+	# sm70 for V100
+
 	cmake -B src/build src \
-		--install-prefix="$(pwd)/hemelabgpu${VARIENT:+-$VARIENT}" \
+		--install-prefix="${INSTALL_DIR}" \
 		--fresh \
 		-DHEMELB_GPU_BACKEND=CUDA \
-		-DCMAKE_CUDA_ARCHITECTURES=70 \
-		"$OPTION"
+		-DCMAKE_BUILD_TYPE=Release \
+		-DCMAKE_C_COMPILER=mpicc \
+		-DCMAKE_CXX_COMPILER=mpicxx \
+		-DCMAKE_CUDA_COMPILER="$(which nvcc)" \
+		-DCMAKE_CUDA_HOST_COMPILER="$(which g++)" \
+		-DCMAKE_CUDA_ARCHITECTURES="${CUVER}" \
+		\
+		-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
+		-DCMAKE_SKIP_BUILD_RPATH=FALSE \
+		-DCMAKE_INSTALL_RPATH="${INSTALL_DIR}/lib" \
+		-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE \
+		\
+		"$@"
 	cmake --build src/build -j
 	cmake --install src/build
 }
